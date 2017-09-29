444f951e26b94776448acd83670c67f6e4f80973
Joshua K. Martinez <joshkmartinez@gmail.com>
2017-09-28 22:45:44 -0700

    Added redux for later

--- src/screens/main.js
+++ src/screens/main.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native'; //must import stuff
 import { Calendar, CalendarList, Agenda } from 'react-native-calendars'; //for calander component
+import { createStore } from "redux"; //redux
 
 export default class Main extends Component {
     render () {
